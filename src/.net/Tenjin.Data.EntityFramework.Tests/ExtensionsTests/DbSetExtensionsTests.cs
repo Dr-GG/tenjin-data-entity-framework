79f72169550d741059b9cfc2e3068e46abf29857
Gerard Gouws <gerard.gouws@capgemini.com>
2022-05-02 14:20:17 +0200

    Added remove and attachment extensions
    20220502 - Added remove and attachment extensions for DbSet and DbContext.

--- src/.net/Tenjin.Data.EntityFramework.Tests/ExtensionsTests/DbSetExtensionsTests.cs
+++ src/.net/Tenjin.Data.EntityFramework.Tests/ExtensionsTests/DbSetExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
@@ -19,6 +20,35 @@ namespace Tenjin.Data.EntityFramework.Tests.ExtensionsTests
             await dbSet.RemoveAsync(toRemoveObject);
 
             mockDbSet.Verify(m => m.Remove(toRemoveObject), Times.Once);
+            mockDbSet.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task RemoveRangeAsync_WhenInvokedWithANullCollection_InvokesTheRemoveRange()
+        {
+            var mockDbSet = new Mock<DbSet<object>>();
+            var dbSet = mockDbSet.Object;
+
+            await dbSet.RemoveRangeAsync(null);
+
+            mockDbSet.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task RemoveRangeAsync_WhenInvokedWithANonNullCollection_InvokesTheRemoveRange()
+        {
+            var mockDbSet = new Mock<DbSet<object>>();
+            var dbSet = mockDbSet.Object;
+            IEnumerable<object> toRemoveObjects = new []
+            {
+                new object(),
+                new object(),
+                new object()
+            };
+
+            await dbSet.RemoveRangeAsync(toRemoveObjects);
+
+            mockDbSet.Verify(m => m.RemoveRange(toRemoveObjects), Times.Once);
             mockDbSet.VerifyNoOtherCalls();
         }
     }
---
+++ src/.net/Tenjin.Data.EntityFramework.Tests/Utilities/DbContext/Models/ComplexPersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tenjin.Data.EntityFramework.Tests.Utilities.DbContext.Models
+{
+    public class ComplexPersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
