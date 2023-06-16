2ba6f09e13c756d8c469f16ec12fcc2e6a493994
Gerard Gouws <gerard.gouws@capgemini.com>
2023-06-16 10:43:03 +0200

    Added general improvements
    Added general improvements using Resharper.
    Added use of Fluent Assertions in unit tests.
    Added Sonarcloud analysis support.
    Added better and more re-usable Azure yaml files.
    Updated nuget packages.

--- src/.net/Tenjin.Data.EntityFramework/Extensions/DbContextExtensions.cs
+++ src/.net/Tenjin.Data.EntityFramework/Extensions/DbContextExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace Tenjin.Data.EntityFramework.Extensions;
 
+/// <summary>
+/// A collection of extension methods for the DbContext instance.
+/// </summary>
 public static class DbContextExtensions
 {
+    /// <summary>
+    /// Attaches an entity in a DbContext as modified.
+    /// </summary>
     public static Task AttachAsModified<TEntity>(this DbContext dbContext,
         TEntity entity,
         Func<TEntity, bool>? removeExistingPredicate = null) where TEntity : class
--- src/.net/Tenjin.Data.EntityFramework/Extensions/DbSetExtensions.cs
+++ src/.net/Tenjin.Data.EntityFramework/Extensions/DbSetExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace Tenjin.Data.EntityFramework.Extensions;
 
+/// <summary>
+/// A collection of extension methods for the DbSet instance.
+/// </summary>
 public static class DbSetExtensions
 {
+    /// <summary>
+    /// Removes a single entity from a DbSet instance.
+    /// </summary>
     public static Task RemoveAsync<TEntity>(this DbSet<TEntity> dbSet, TEntity entity) where TEntity : class
     {
         dbSet.Remove(entity);
@@ -11,6 +17,9 @@ public static class DbSetExtensions
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Removes a range of entities from a DbSet instance.
+    /// </summary>
     public static Task RemoveRangeAsync<TEntity>(this DbSet<TEntity> dbSet, IEnumerable<TEntity>? entities) where TEntity : class
     {
         if (entities == null)
